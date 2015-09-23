87ca5f62ad933992c74a31b200e1a745c3c760c2
Toni Oliver <toliver.shadow@gmail.com>
2015-09-23 20:35:08 +0100

    First working version

---
+++ optoforce/src/optoforce/optoforce.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+# Copyright 2015 Shadow Robot Company Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import serial
+import struct
+import binascii
+import rospy
+from geometry_msgs.msg import WrenchStamped
+
+
+class OptoforceData:
+    def __init__(self):
+        self.status = None
+        self.force = []
+
+
+class OptoforceDriver(object):
+    """
+    Driver for Optoforce sensors
+    """
+    _sensor_frame_length = {'31': 16,
+                            '34': 34,
+                            '64': 22}
+
+    def __init__(self):
+        """
+        Initialize OptoforceDriver object
+        @param name - name of the MoveIt group
+        """
+        self._serial = serial.Serial('/dev/ttyACM0', 1000000, timeout=0.5)
+        self._sensor_type = '34'
+        self._publishers = []
+        self._wrenches = []
+
+        if self._sensor_type == '34':
+            for i in range(4):  # 4 sensors
+                self._publishers.append(rospy.Publisher("optoforce_" + str(i), WrenchStamped, queue_size=100))
+                wrench = WrenchStamped()
+                wrench.header.frame_id = "optoforce_" + str(i)
+                self._wrenches.append(wrench)
+
+    def config(self):
+        # TODO Send config parameters
+        pass
+
+    def run(self):
+        """
+        Runs the read loop.
+        """
+        while(True):
+            s = self._serial.read(self._sensor_frame_length[self._sensor_type])
+            data = self._decode(s)
+            self._publish(data)
+            # print binascii.hexlify(s)
+
+    def _decode(self, frame):
+        if not self._is_checksum_valid(frame):
+            return None
+
+        data = OptoforceData()
+        offset = 6
+        data.status = struct.unpack_from('<H', frame, offset)[0]
+
+        if self._sensor_type == '34':
+            for _ in range(4):  # 4 sensors
+                force_axes = []
+                for __ in range(3):  # 3 axes per sensor
+                    offset += 2
+                    val = struct.unpack_from('<h', frame, offset)[0]
+                    # TODO Convert to Newtons (needs sensitivity report)
+                    val = float(val) / 10000
+                    force_axes.append(val)
+                data.force.append(force_axes)
+        return data
+
+    def _publish(self, data):
+        stamp = rospy.Time.now()
+        for i in range(4):  # 4 sensors
+            self._wrenches[i].header.stamp = stamp
+            self._wrenches[i].wrench.force.x = data.force[i][0]
+            self._wrenches[i].wrench.force.y = data.force[i][1]
+            self._wrenches[i].wrench.force.z = data.force[i][2]
+            self._publishers[i].publish(self._wrenches[i])
+
+    @staticmethod
+    def _is_checksum_valid(frame):
+        # TODO Compute and check checksum
+        return True
+
+if __name__ == '__main__':
+    rospy.init_node("optoforce")
+    driver = OptoforceDriver()
+    driver.run()
