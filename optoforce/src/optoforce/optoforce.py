0188fa1b33a0d5d436bee9476ec0b2dc41a9e0b3
Dorian Goepp <dorian.goepp@inria.fr>
2016-07-28 18:08:28 +0200

    first draft

--- optoforce/src/optoforce/optoforce.py
+++ optoforce/src/optoforce/optoforce.py
@@ -21,6 +21,7 @@ import serial
 import struct
 import binascii
 import rospy
+import std_srvs.srv
 from geometry_msgs.msg import WrenchStamped
 
 
@@ -84,7 +85,7 @@ class OptoforceDriver(object):
         self._wrenches = []
         self._nb_sensors = 0
         self._nb_axis = 0
-        
+
         if self._sensor_type == self._OPTOFORCE_TYPE_31:
             self._nb_sensors = 1
             self._nb_axis = 3
@@ -94,15 +95,15 @@ class OptoforceDriver(object):
         elif self._sensor_type == self._OPTOFORCE_TYPE_64:
             self._nb_sensors = 1
             self._nb_axis = 6
-            
+
         self._scale = rospy.get_param("~scale")
 
         if len(self._scale) != self._nb_sensors:
-            raise ValueError("Number of sensors [%i]and scaling factor vectors [%i] given doesn't match." % (self._nb_sensors, len(self._scale))) 
+            raise ValueError("Number of sensors [%i]and scaling factor vectors [%i] given doesn't match." % (self._nb_sensors, len(self._scale)))
         else:
             for x in range(self._nb_sensors):
                 if len(self._scale[x]) != self._nb_axis:
-                    raise ValueError("Number of axis [%i] and scaling factors [%i] given doesn't match." % (self._nb_axis, len(self._scale[x]))) 
+                    raise ValueError("Number of axis [%i] and scaling factors [%i] given doesn't match." % (self._nb_axis, len(self._scale[x])))
 
         for i in range(self._nb_sensors):
             self._publishers.append(rospy.Publisher("optoforce_" + str(self._starting_index + i), WrenchStamped,
@@ -111,13 +112,40 @@ class OptoforceDriver(object):
             wrench.header.frame_id = "optoforce_" + str(self._starting_index + i)
             self._wrenches.append(wrench)
 
+        # Advertise a service to retrieve the unique ID of the sensor
+        self._service = rospy.Service('get_unique_id', std_srvs.srv.Empty, self.get_unique_id)
+
+        # Tree representation of all accepted headers.
+        # Leaf nodes are the lenght of the frame associated with the header
+        self._headers = {
+            170:
+                {0:
+                    {18:
+                        {8: 14}},
+                7:
+                    {8:
+                        {10: 16,
+                        16: 34,
+                        28: 22}}}}
+        # self._headers = {
+        #     struct.pack('>B', 171):
+        #         {struct.pack('>B', 0):
+        #             {struct.pack('>B', 18):
+        #                 {struct.pack('>B', 8): 14}}},
+        #     struct.pack('>B', 170):
+        #         {struct.pack('>B', 7):
+        #             {struct.pack('>B', 8):
+        #                 {struct.pack('>B', 10): 16,
+        #                 struct.pack('>B', 16): 34,
+        #                 struct.pack('>B', 28): 22}}}}
+
     def config(self):
         speed = self._speed_values[rospy.get_param("~speed", "100Hz")]
         filter = self._filter_values[rospy.get_param("~filter", "15Hz")]
         zero = self._zeroing_values[rospy.get_param("~zero", "false")]
         config_length = 9
 
-        header = self._frame_header()
+        header = struct.pack('>4B', 170, 0, 50, 3)
         offset = 0
 
         frame = array.array('B', [0] * config_length)
@@ -130,16 +158,57 @@ class OptoforceDriver(object):
 
         self._serial.write(frame)
 
+    def get_unique_id(self, request):
+        config_length = 6
+        offset = 0
+
+        # Build the frame and send it
+        frame = array.array('B', [0] * config_length)
+        struct.pack_into('>6B', frame, offset, 171, 0, 18, 8, 0, 197)
+        self._serial.write(frame)
+
+        return std_srvs.srv.EmptyResponse()
+
     def run(self):
         """
         Runs the read loop.
         """
+        sizes_seen = []
+
         while not rospy.is_shutdown():
-            s = self._serial.read(self._sensor_frame_length[self._sensor_type])
-            rospy.logdebug(binascii.hexlify(s))
-            data = self._decode(s)
-            if data:
-                self._publish(data)
+            data = self._detect_header(self._headers)
+            if len(data) > 1 and len(data) not in sizes_seen:
+                sizes_seen.append(len(data))
+                format_string = '>' + str(len(data)) + 'B'
+                rospy.logdebug(format_string)
+                rospy.logdebug("frame s: " + str(struct.unpack(format_string, data)))
+
+            if len(data) > 1:
+                self._decode(data)
+            else:
+                rospy.logwarn("frame with unknown header received")
+
+            # s = self._serial.read(self._sensor_frame_length[self._sensor_type])
+            # rospy.logdebug(binascii.hexlify(s))
+            # data = self._decode(s)
+            # if data:
+            #     self._publish(data)
+
+    def _detect_header(self, tree):
+        raw_byte = self._serial.read()
+        byte = struct.unpack('>B', raw_byte)[0]
+
+        for header_byte, subtree in tree.items():
+            if byte == header_byte:
+                if type(subtree) is int:
+                    return raw_byte + self._serial.read(subtree-4)
+                else:
+                    next_bytes = self._detect_header(subtree)
+                    if next_bytes: # hacky
+                        return raw_byte + next_bytes
+                    else:
+                        return '' # hacky
+        return '' # hacky
 
     def _decode(self, frame):
         """
@@ -155,24 +224,33 @@ class OptoforceDriver(object):
             # of the loss of frame synchronisation
             # This method would be wrong if the cause were an actual transmission error, but this doesn't
             # seem to happen.
-            s = self._serial.read(self._config_response_frame_length)
+            # FIXME: s = self._serial.read(self._config_response_frame_length)
             return None
 
-        data = OptoforceData()
-        offset = 6
-        data.status = struct.unpack_from('>H', frame, offset)[0]
+        header = struct.unpack_from('>4B', frame)
 
-        for s in range(self._nb_sensors):
-            force_axes = []
-            for a in range(self._nb_axis):
-                offset += 2
-                val = struct.unpack_from('>h', frame, offset)[0]
-                # TODO Convert to Newtons (needs sensitivity report)
-                val = float(val) / self._scale[s][a]
-                force_axes.append(val)
-            data.force.append(force_axes)
+        data_headers = [(170, 7, 8, 10), (170, 7, 8, 16), (170, 7, 8, 28)]
+        if header in data_headers:
+            data = OptoforceData()
+            offset = 6
+            data.status = struct.unpack_from('>H', frame, offset)[0]
 
-        return data
+            for s in range(self._nb_sensors):
+                force_axes = []
+                for a in range(self._nb_axis):
+                    offset += 2
+                    val = struct.unpack_from('>h', frame, offset)[0]
+                    # TODO Convert to Newtons (needs sensitivity report)
+                    val = float(val) / self._scale[s][a]
+                    force_axes.append(val)
+                data.force.append(force_axes)
+
+            return data
+        elif header == (170, 0, 18, 8):
+            rospy.logdebug("header match for unique identifier!")
+            offset = 4
+            string = struct.unpack_from('>8c', frame, offset)
+            print(''.join(string))
 
     def _publish(self, data):
         stamp = rospy.Time.now()
@@ -207,13 +285,9 @@ class OptoforceDriver(object):
         checksum = struct.unpack_from('>H', frame, offset)[0]
         return calculated == checksum
 
-    @staticmethod
-    def _frame_header():
-        return struct.pack('>4B', 170, 0, 50, 3)
-
-
 if __name__ == '__main__':
-    rospy.init_node("optoforce")
+    rospy.init_node("optoforce", log_level=rospy.DEBUG)
     driver = OptoforceDriver()
     driver.config()
     driver.run()
+    # rospy.spin()
